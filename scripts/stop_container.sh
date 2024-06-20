958917c78e5b5794fe617fc2137ea1158bc676e4
Abed Aktar <79903434+aktar-75@users.noreply.github.com>
2024-06-20 19:29:24 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -5,6 +5,6 @@ containerID=$(docker ps -q --filter "name=my-container")
 
 # Stop the container if it is running
 if [ -n "$containerID" ]; then
-  docker stop "$containerID"
+  docker stop "$containerID" 
 fi  
 
